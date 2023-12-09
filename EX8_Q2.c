2d32f0b34c47c78b61b1cdfc9170523121b0fa16
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-12-09 16:37:22 +0530

    Experiment 8 Question 2
    Write a program to multiply two matrix using functions

---
+++ EX8_Q2.c
@@ -0,0 +1,70 @@
+// Write a program to multiply two matrix using functions
+
+#include <stdio.h>
+
+void matrixMul(int a[][10], int b[][10], int r1, int c1)
+{
+    int ans[r1][c1];
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            ans[i][j] = 0;
+            for (int k = 0; k < r1; k++)
+            {
+                ans[i][j] = a[i][k] * b[k][j];
+            }
+            printf("\n");
+        }
+    }
+    for (int i = 0; i < r1; i++)
+    {
+        for (int j = 0; j < c1; j++)
+        {
+            printf("%d ",ans[i][j]);
+        }
+    }
+}
+
+int main()
+{
+    int r1, c1, r2, c2;
+    printf("\nEnter row for matrix 1 :- ");
+    scanf("%d", &r1);
+    printf("\nEnter column for matrix 1 :- ");
+    scanf("%d", &c1);
+    printf("\nEnter row for matrix 2 :- ");
+    scanf("%d", &r2);
+    printf("\nEnter column for matrix 2 :- ");
+    scanf("%d", &c2);
+
+    int mat1[r1][10];
+    int mat2[r2][10];
+
+    if (c1 == r2)
+    {
+        printf("Enter Matrix 1 Values :- ");
+        for (int i = 0; i < r1; i++)
+        {
+            for (int j = 0; j < c1; j++)
+            {
+                scanf("%d", &mat1[i][j]);
+            }
+        }
+        printf("Enter Matrix 2 Values :- ");
+        for (int i = 0; i < r2; i++)
+        {
+            for (int j = 0; j < c2; j++)
+            {
+                scanf("%d", &mat2[i][j]);
+            }
+        }
+        printf("\nMatrix Multiplication:\n");
+        matrixMul(mat1, mat2, r1, c1);
+    }
+    else
+    {
+        printf("Multiplication not possible. c1 != r2\n");
+    }
+    return 0;
+}
