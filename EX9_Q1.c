accbc181c81699827f96a7be5c956ef299cd72c1
Ashutosh <90296517+Ashutosh021@users.noreply.github.com>
2023-12-18 21:57:27 +0530

    Experiment 9 Question 1
    Write a C program to add two numbers using pointers.

---
+++ EX9_Q1.c
@@ -0,0 +1,21 @@
+// Write a C program to add two numbers using pointers.
+
+#include <stdio.h>  
+  
+int main() {  
+    int num1,num2;  
+    int *p1,*p2; 
+    printf("Enter num1 : ");
+    scanf("%d",&num1);
+    printf("Enter num2 : ");
+    scanf("%d",&num2);
+    p1 = &num1;  
+    p2 = &num2;  
+    int sum;  
+  
+    sum = *p1 + *p2;  
+  
+    printf("Sum of %d and %d is: %d\n", *p1, *p2, sum);  
+  
+    return 0;  
+}  
